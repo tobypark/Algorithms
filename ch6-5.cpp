a60bf0a326cc58e4f0c13db501ae09b54b729a88
Hwi Won Park <63436036+HwiWonPark@users.noreply.github.com>
2020-10-19 14:21:12 +0900

    소스 추가

---
+++ ch6-5.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+void insertion(int a[], int n)
+{
+	int i, j;
+	for (i = 1; i < n; i++) {
+		int tmp = a[i];
+		for (j = i; j > 0 && a[j - 1] > tmp; j--)
+			a[j] = a[j - 1];
+		a[j] = tmp;
+	}
+}
+
+int main(void)
+{
+	int i, nx;
+	int* x;
+	puts("단순 삽입 정렬");
+
+	printf("요소 개수 : ")
+	scanf("%d", &nx);
+	x = calloc(nx, sizeof(int));
+
+	for (i = 0; i < nx; i++) {
+		printf("x[%d] : ", i);
+		scanf("%d", &x[i]);
+	}
+
+	insertion(x, nx);
+
+	puts("오름차순으로 정렬했습니다.");
+	for (i = 0; i < nx; i++)
+		printf("x[%d] = %d\n", i, x[i]);
+
+	free(x);
+
+	return 0;
+}
